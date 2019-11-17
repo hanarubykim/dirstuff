a3b6d22025147cf3028071e2f58ded4459ad7cdf
Hana Kim <hanarubykim2002@gmail.com>
2019-11-16 22:12:23 -0500

    fixing

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <dirent.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -13,41 +14,39 @@ void list(DIR *directory){
   long totalSize = 0;
 
   while(currentFile = readdir(directory)){
-    files++;
-    printf("File %d: %s is ", fileNum, currentFile->d_name)
-    if(currentFile->d_type == 4){
-      printf("a directory.\n");
-    }
-    else{
-      printf("not a directory.\n");
-      stat(currentFile->d_name, &x);
-      totalSize += x.st_size;
-    }
+    fileNum++;
+    printf("File %d: %s is ", fileNum, currentFile->d_name);
+    // if(currentFile->d_type == 4){
+    //   printf("a directory.\n");
+    // }
+    // else{
+    //   printf("not a directory.\n");
+    //   stat(currentFile->d_name, &x);
+    //   totalSize += x.st_size;
+    // }
   }
+  int i = 0;
   char * byteSize[] = {"B, KB, MB, GB"};
   while(totalSize >= 1024){
     totalSize /= 1024;
     i++;
   }
   // Print out the size in a more readable format (like using KB, MB, GB for -byte prefixes)
-  printf("The size of all the regular files of this directory is %ld%s\n\n", tmp, byteSize[i]);
+  printf("The size of all the regular files of this directory is %ld%s\n\n", totalSize, byteSize[i]);
 }
 
 int main(){
   DIR *toad;
-  toad = openddir("dirstuff");
+  toad = opendir("dirstuff");
   if(toad == NULL){
     printf("Unable to read directory.");
-    closeddir(toad);
+    closedir(toad);
   }else{
     // list all the files in the directory
     // specify which files are directories (if any)
     // show the total size of all the regular files the directory
     list(toad);
   }
+  closedir(toad);
 // Have the program scan the current directory
 }
-
-// Possible enhancements:
-// Recursively list the files in any subdirectories, update the total to include the total size of subdirectories
-// Make it look more like $ ls -l
