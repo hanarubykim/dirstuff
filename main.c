5d4bbf016cfb86322eb2a3c9daff79f1412808cf
Hana Kim <hanarubykim2002@gmail.com>
2019-11-16 21:44:16 -0500

    finished up program

--- main.c
+++ main.c
@@ -5,16 +5,16 @@
 
 // list all the files in the directory
 // specify which files are directories (if any)
+// show the total size of all the regular files the directory
 void list(DIR *directory){
   struct dirent *currentFile;
   struct stat x;
   int fileNum = 0;
-  int totalSize = 0;
+  long totalSize = 0;
 
   while(currentFile = readdir(directory)){
     files++;
     printf("File %d: %s is ", fileNum, currentFile->d_name)
-
     if(currentFile->d_type == 4){
       printf("a directory.\n");
     }
@@ -24,11 +24,18 @@ void list(DIR *directory){
       totalSize += x.st_size;
     }
   }
+  char * byteSize[] = {"B, KB, MB, GB"};
+  while(totalSize >= 1024){
+    totalSize /= 1024;
+    i++;
+  }
+  // Print out the size in a more readable format (like using KB, MB, GB for -byte prefixes)
+  printf("The size of all the regular files of this directory is %ld%s\n\n", tmp, byteSize[i]);
 }
 
 int main(){
   DIR *toad;
-  toad = openddir(".");
+  toad = openddir("dirstuff");
   if(toad == NULL){
     printf("Unable to read directory.");
     closeddir(toad);
@@ -38,13 +45,9 @@ int main(){
     // show the total size of all the regular files the directory
     list(toad);
   }
-
-
 // Have the program scan the current directory
-
-
 }
+
 // Possible enhancements:
 // Recursively list the files in any subdirectories, update the total to include the total size of subdirectories
-// Print out the size in a more readable format (like using KB, MB, GB for -byte prefixes)
 // Make it look more like $ ls -l
